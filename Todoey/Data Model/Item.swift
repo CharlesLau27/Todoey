1ff97eecab17b54ee89335cc8644af6cade23d68
Charles Lau <CharlesLau@charless-macbook-pro.local>
2018-07-29 16:49:14 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Charles Lau on 2018-07-28.
+//  Copyright © 2018 ABC. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
+
+
